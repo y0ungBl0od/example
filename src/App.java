624a33b90b5f492ec8b2f15b6aae5165afd282a4
y0ungBl0od <124034812+y0ungBl0od@users.noreply.github.com>
2024-05-01 23:07:17 +0300

    commit from local

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import controller.Controller;
+import model.Model;
+import view.View;
+
+
+public class App {
+    public static void main(String[] args){
+        Model model = new Model();
+        View view = new View();
+        Controller controller = new Controller();
+    }
+}
